1a5d92523478fb2285b2c6cc4939225db086c11e
sowmya.ramesh <sowr94@gmail.com>
2024-12-30 20:54:34 +0530

    Build and Deploy

--- crewmeister-helm-chart/resource.tf
+++ crewmeister-helm-chart/resource.tf
@@ -3,11 +3,23 @@ resource "helm_release" "crewmeister_application" {
   namespace = "default"
   chart     = "/home/runner/work/devops-coding-challenge/devops-coding-challenge/crewmeister-helm-chart"
   version   = "0.2.3"
-  values =  [
+
+  values = [
     <<EOF
 service:
   type: ClusterIP
-  port: 8080
+  port: 8080  # Ensure this is set
+
+ingress:
+  enabled: false  # Add this line to resolve ingress.enabled error
+  className: ""
+  annotations: []
+  hosts:
+    - host: crewmeister-app.local
+      paths:
+        - path: /
+          pathType: ImplementationSpecific
+  tls: []
 
 serviceAccount:
   create: true
