bf91736952fcbcc5b8a1379fe1260f9078b13886
sowmya.ramesh <sowr94@gmail.com>
2024-12-30 21:51:15 +0530

    Build & Deploy

--- crewmeister-helm-chart/resource.tf
+++ crewmeister-helm-chart/resource.tf
@@ -3,7 +3,7 @@ resource "helm_release" "crewmeister_application" {
   namespace = "default"
   chart     = "/home/runner/work/devops-coding-challenge/devops-coding-challenge/crewmeister-helm-chart"
   version   = "0.2.3"
-  values = [file("/home/runner/work/devops-coding-challenge/devops-coding-challenge/crewmeister-helm-chart/crewmeister-values.yaml")]
+  values = [file("/home/runner/work/devops-coding-challenge/devops-coding-challenge/crewmeister-helm-chart/values.yaml")]
   set {
     name  = "service.port"
     value = "8080"  # Or another value for the port if you want to override it
