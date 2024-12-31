b4d86ce643502df3f50feeb96bf22de6e2f39870
sowmya.ramesh <sowr94@gmail.com>
2024-12-31 21:57:07 +0530

    update

--- crewmeister-helm-chart/provider.tf
+++ crewmeister-helm-chart/provider.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = "/home/runner/.kube/config"
   config_context = "minikube"
 }
 
--- helm/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-  config_context = "minikube"
-}
-
-output "helm_release_name" {
-  value = helm_release.crewmeister_app.name
-}
