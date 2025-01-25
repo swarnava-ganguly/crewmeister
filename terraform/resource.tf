b54b49d2c1d29c0525007e65d89d88f77bb5f8e5
Your Name <you@example.com>
2025-01-26 00:31:57 +0530

    wip

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -10,7 +10,8 @@ resource "helm_release" "crewmeister_application" {
   namespace = "default"
   chart     = "C:/Users/user/OneDrive/Desktop/project/crewmeister/helm"
   version   = "0.2.3"
-  values = [file("C:/Users/user/OneDrive/Desktop/project/crewmeister/helm/values.yaml")]
+  # values = [file("C:/Users/user/OneDrive/Desktop/project/crewmeister/helm/values.yaml")]
+  values = [file("/home/runner/crewmeister/helm/values.yaml")]
 #   set {
 #     name  = "service.port"
 #     value = "8080"  # Or another value for the port if you want to override it
