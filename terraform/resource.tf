d8cfe81acf393ed63ee878d031c47efad1add774
Your Name <you@example.com>
2025-01-26 11:16:58 +0530

    wip

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -12,9 +12,9 @@ resource "helm_release" "crewmeister_application" {
   version   = "0.2.3"
   # values = [file("C:/Users/user/OneDrive/Desktop/project/crewmeister/helm/values.yaml")]
   values = [file("/home/runner/work/crewmeister/crewmeister/helm/values.yaml")]
-#   set {
-#     name  = "service.port"
-#     value = "8080"  # Or another value for the port if you want to override it
-#   }
+  set {
+    name  = "javaApplication.image.tag"
+    value = "76487789b97848f1b92a27100282e32de02f2978"  # Or another value for the port if you want to override it
+  }
 }
 
