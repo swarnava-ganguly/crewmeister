f737d38c82a1fc6a98737f5e99b8a04b3f6fbb72
Your Name <you@example.com>
2025-01-29 12:41:37 +0530

    Final Pipeline

--- terraform/modules/helmtf/provider.tf
+++ terraform/modules/helmtf/provider.tf
@@ -1,12 +1,12 @@
 provider "kubernetes" {
   config_path = "${var.config_path}"
-  # config_context = "${var.config_context}"
+  # config_context = "${var.config_context}" # use the context when needed
 }
 
 provider "helm" {
   kubernetes {
     config_path = "${var.config_path}"
-    # config_context = "${var.config_context}"
+    # config_context = "${var.config_context}" # use the context when needed
   }
 }
 
